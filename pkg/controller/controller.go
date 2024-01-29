e971a4e8ceb8df669bc4c82b644f852beebfe4a2
EJ Etherington <ejether@hotmail.com>
2024-01-29 13:10:31 -0800

    Linting fixes, starting automation

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package controller
 
 import (
@@ -41,9 +58,13 @@ func WatchNamespaces(namespacesConfig *knamespace.NamespacesConfig) {
 
 	for event := range watcher.ResultChan() {
 		item := event.Object.(*corev1.Namespace)
-		log.Infof("Caught event %s on namespace: %s. Processing...", event.Type, item.GetName())
+		namespaceName := item.GetName()
+		log.Infof("Caught event %s on namespace: %s. Processing...", event.Type, namespaceName)
 
-		processNamespace(item.GetName(), namespacesConfig)
+		err := processNamespace(namespaceName, namespacesConfig)
+		if err != nil {
+			log.Errorf("Encounter %s while processing %s. Skipping....", err, namespaceName)
+		}
 	}
 }
 
@@ -60,7 +81,7 @@ func processNamespace(namespaceName string, namespacesConfig *knamespace.Namespa
 
 	namespace, err := kube.GetClusterNamespace(namespaceName)
 	if err != nil {
-		log.Infof("Unable to fetch cluster namespace '%s' for modification: %s")
+		log.Infof("Unable to fetch cluster namespace '%s' for modification: %s", namespaceName, err)
 		return err
 	}
 
--- pkg/knamespace/knamespace.go
+++ pkg/knamespace/knamespace.go
@@ -1,7 +1,23 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package knamespace
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
@@ -29,7 +45,7 @@ func (n NamespacesConfig) GetConfig(namespaceName string) (*NamespaceConfig, err
 			return &namespaceConfig, nil
 		}
 	}
-	return nil, errors.New(fmt.Sprintf("namespace %s not found in configuration", namespaceName))
+	return nil, fmt.Errorf(fmt.Sprintf("namespace %s not found in configuration", namespaceName))
 }
 
 // Get Knamespacer Defaults
@@ -40,7 +56,7 @@ func (n NamespacesConfig) GetDefault() (*NamespaceConfig, error) {
 // Read and return namespaces configuration. Exit if we can't.
 func GetNamespacesConfig(namespacesConfigFileName string) *NamespacesConfig {
 	data := &NamespacesConfig{}
-	log.Info("Reading Config File %s:", namespacesConfigFileName)
+	log.Infof("Reading Config File %s:", namespacesConfigFileName)
 	contents, err := os.ReadFile(namespacesConfigFileName)
 	if err != nil {
 		log.Fatalf("Error reading Knamespacer Configuration File %s: %s", namespacesConfigFileName, err)
